58b265e2903e96d28e71b286515d0674ba79a03b
Brandon Cox <brandon.cox.519@gmail.com>
2019-03-23 14:20:06 -0400

    adding the golang imagestream

--- knative-kubecon/scripts/preload.sh
+++ knative-kubecon/scripts/preload.sh
@@ -2,10 +2,12 @@
 set -e
 set -x
 
-eval "$(minishift docker-env)"
+
 docker image pull centos/go-toolset-7-centos7:latest
 docker image pull vdemeester/kobw-builder:0.1.1
 for img in $(oc get image.caching.internal.knative.dev -ojsonpath='{range .items[*]}{.spec.image}{"\n"}{end}' --all-namespaces)
 do
     docker pull $img
 done
+
+oc create -f https://raw.githubusercontent.com/sclorg/golang-container/master/imagestreams/golang-centos7.json -n openshift
